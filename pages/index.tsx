4b742579972ca5506b73fef6d4c9dc31376a2ccf
Ohjin Kwon <lazy134407@gmail.com>
2022-03-26 18:45:36 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import type { NextPage } from 'next';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from 'common/components/link/Link';
+import Copyright from '../common/components/copy-right/Copyright';
+
+const Home: NextPage = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          mine
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
