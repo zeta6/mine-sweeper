ccd2457119e20f3df051f720920153a5ed8db8a6
Ohjin Kwon <lazy134407@gmail.com>
2022-04-09 02:23:24 +0900

    ADD test api address for test getMines func

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -4,20 +4,43 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log('req', req)
-  res.send({ test: 'abcd' })
-  // const postId = req.query.id
+  console.log('reqMT', req.body.mineTotal)
+  console.log('reqST', req.body.squareTotal)
 
-  // if (req.method === 'GET') {
-  //   handleGET(postId, res)
-  // } else if (req.method === 'DELETE') {
-  //   handleDELETE(postId, res)
-  // } else {
-  //   throw new Error(
-  //     `The HTTP ${req.method} method is not supported at this route.`
-  //   )
-  // }
+  const getRandomInt = (min: number, max: number) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  const getMines = (mineTotal: number, squareTotal: number): Array<number> => {
+    let mineCount = 0
+    const mines: number[] = []
+    while (mineCount < mineTotal) {
+      const mine = getRandomInt(0, squareTotal)
+      if (!mines.includes(mine)) {
+        mines.push(mine)
+        mineCount = mineCount + 1
+      }
+    }
+    return mines
+  }
+  const mines = getMines(req.body.mineTotal, req.body.squareTotal)
+  console.log('mines', mines)
+  res.send({ mines: mines })
 }
+
+// const postId = req.query.id
+
+// if (req.method === 'GET') {
+//   handleGET(postId, res)
+// } else if (req.method === 'DELETE') {
+//   handleDELETE(postId, res)
+// } else {
+//   throw new Error(
+//     `The HTTP ${req.method} method is not supported at this route.`
+//   )
+// }
 
 // // GET /api/post/:id
 // async function handleGET(postId, res) {
