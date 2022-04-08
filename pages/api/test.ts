744908de5e03a1babd78d0f18d94d8444dbae1a9
Ohjin Kwon <lazy134407@gmail.com>
2022-04-09 02:27:50 +0900

    ADD test api address for test getMines func

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,32 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getMines } from 'common/utils/game/getMines'
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log('reqMT', req.body.mineTotal)
-  console.log('reqST', req.body.squareTotal)
-
-  const getRandomInt = (min: number, max: number) => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min
-  }
-
-  const getMines = (mineTotal: number, squareTotal: number): Array<number> => {
-    let mineCount = 0
-    const mines: number[] = []
-    while (mineCount < mineTotal) {
-      const mine = getRandomInt(0, squareTotal)
-      if (!mines.includes(mine)) {
-        mines.push(mine)
-        mineCount = mineCount + 1
-      }
-    }
-    return mines
-  }
   const mines = getMines(req.body.mineTotal, req.body.squareTotal)
-  console.log('mines', mines)
   res.send({ mines: mines })
 }
 
